bf9e3ec8a2e12eb680c6ed67f2bb143a4c835d2f
Jingwei Zhang <z1771485029@gmail.com>
2025-06-28 17:51:33 +0800

    update

--- tools/docs/docusaurus.config.ts
+++ tools/docs/docusaurus.config.ts
@@ -81,7 +81,7 @@ const config: Config = {
         },
         {to: '/blog', label: 'Blog', position: 'left'},
         {
-          href: 'https://github.com/facebook/docusaurus',
+          href: 'https://github.com/CurvatureX/ai-tutor-monorepo',
           label: 'GitHub',
           position: 'right',
         },
